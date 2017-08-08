a089edea02f526d45e2c06a45795483d2901cd20
Keane <keanemind@gmail.com>
2017-08-07 20:58:21 -0500

    Change quiz games to be channel based
    This solves the problem of quiz game always sending messages to the server's default channel. This also allows multiple quiz games to be played simultaneously on the same server.

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -23,7 +23,7 @@ class Quiz:
         self.bot = bot
         self.save_file = dataIO.load_json(SAVE_FILEPATH)
 
-        self.playing_servers = {}
+        self.playing_channels = {}
         self.timeout = 20
         self.game_tasks = []
 
@@ -44,79 +44,78 @@ class Quiz:
     @quiz.command(name="play", pass_context=True)
     async def quiz_play(self, ctx):
         """Create or join a quiz game."""
-        server = ctx.message.server
+        channel = ctx.message.channel
         player = ctx.message.author
-        if server.id not in self.playing_servers:
-            self.playing_servers[server.id] = {"Start":datetime.datetime.utcnow(),
-                                               "Started":False,
-                                               "Players":{player.id:0},
-                                               "Answers":{}
-                                              }
+        if channel.id not in self.playing_channels:
+            self.playing_channels[channel.id] = {"Start":datetime.datetime.utcnow(),
+                                                 "Started":False,
+                                                 "Players":{player.id:0},
+                                                 "Answers":{}
+                                                }
             return await self.bot.say("{} is starting a quiz game! It will start "
                                       "in 20 seconds. Use `{}quiz play` to join."
                                       .format(player.display_name, ctx.prefix))
 
-        serverinfo = self.playing_servers[server.id]
-        since_start = (datetime.datetime.utcnow() - serverinfo["Start"]).total_seconds()
-        if player.id in serverinfo["Players"]:
+        channelinfo = self.playing_channels[channel.id]
+        if player.id in channelinfo["Players"]:
             await self.bot.say("You are already in the game.")
-        elif since_start > self.timeout:
+        elif channelinfo["Started"]:
             await self.bot.say("A quiz game is already underway.")
         else:
-            serverinfo["Players"][player.id] = 0
+            channelinfo["Players"][player.id] = 0
             await self.bot.say("{} joined the game.".format(player.display_name))
 
     async def start_loop(self):
         """Starts quiz games when the timeout period ends."""
         while True:
             await asyncio.sleep(1)
-            for serverid in list(self.playing_servers):
-                serverinfo = self.playing_servers[serverid]
-                since_start = (datetime.datetime.utcnow() - serverinfo["Start"]).total_seconds()
-                if since_start > self.timeout and not serverinfo["Started"]:
-                    if len(serverinfo["Players"]) > 1:
-                        server = self.bot.get_server(serverid)
-                        self.game_tasks.append(self.bot.loop.create_task(self.game(server)))
-                        serverinfo["Started"] = True
+            for channelid in list(self.playing_channels):
+                channelinfo = self.playing_channels[channelid]
+                since_start = (datetime.datetime.utcnow() - channelinfo["Start"]).total_seconds()
+                if since_start > self.timeout and not channelinfo["Started"]:
+                    if len(channelinfo["Players"]) > 1:
+                        channel = self.bot.get_channel(channelid)
+                        self.game_tasks.append(self.bot.loop.create_task(self.game(channel)))
+                        channelinfo["Started"] = True
                     else:
-                        await self.bot.send_message(self.bot.get_server(serverid),
+                        await self.bot.send_message(self.bot.get_channel(channelid),
                                                     "Nobody else joined the quiz game.")
-                        self.playing_servers.pop(serverid)
+                        self.playing_channels.pop(channelid)
 
     async def on_message(self, message):
         authorid = message.author.id
-        serverid = message.server.id
+        channelid = message.channel.id
         choice = message.content.lower()
-        if serverid in self.playing_servers:
-            serverinfo = self.playing_servers[serverid]
-            if (authorid in serverinfo["Players"]
-                    and authorid not in serverinfo["Answers"]
+        if channelid in self.playing_channels:
+            channelinfo = self.playing_channels[channelid]
+            if (authorid in channelinfo["Players"]
+                    and authorid not in channelinfo["Answers"]
                     and choice in {"a", "b", "c", "d"}):
-                serverinfo["Answers"][authorid] = {"Choice":choice,
-                                                   "Time":time.perf_counter()}
+                channelinfo["Answers"][authorid] = {"Choice":choice,
+                                                    "Time":time.perf_counter()}
 
-    async def game(self, server):
-        """Runs a quiz game on a server."""
-        self.add_server(server)
+    async def game(self, channel):
+        """Runs a quiz game on a channel."""
+        self.add_server(channel.server)
 
         try:
             category = await self.category_selector()
             category_name = await self.category_name(category)
-            response = await self.get_questions(server, category=category)
+            response = await self.get_questions(channel.server, category=category)
         except RuntimeError:
-            await self.bot.send_message(server, "An error occurred in retrieving questions. "
+            await self.bot.send_message(channel, "An error occurred in retrieving questions. "
                                         "Please try again.")
-            self.playing_servers.pop(server.id)
+            self.playing_channels.pop(channel.id)
             raise
 
-        serverinfo = self.playing_servers[server.id]
+        channelinfo = self.playing_channels[channel.id]
 
         # Introduction
         intro = ("Welcome to the quiz game! Your category is {}.\n"
                  "Remember to answer correctly as quickly as you can. "
                  "You have 10 seconds per question.\n"
                  "The game will begin shortly.".format(category_name))
-        await self.bot.send_message(server, intro)
+        await self.bot.send_message(channel, intro)
         await asyncio.sleep(4)
 
         # Question and Answer
@@ -139,30 +138,30 @@ class Quiz:
             message += "D. {}\n".format(answers[3])
             message += "```"
 
-            message_obj = await self.bot.send_message(server, message)
+            message_obj = await self.bot.send_message(channel, message)
             await self.bot.add_reaction(message_obj, "0⃣")
-            serverinfo["Answers"].clear() # clear the previous question's answers
+            channelinfo["Answers"].clear() # clear the previous question's answers
             start_time = time.perf_counter()
 
             numbers = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣", "🔟"]
             for i in range(10):
-                if len(serverinfo["Answers"]) == len(serverinfo["Players"]):
+                if len(channelinfo["Answers"]) == len(channelinfo["Players"]):
                     break
                 await asyncio.sleep(1)
                 await self.bot.add_reaction(message_obj, numbers[i])
 
             # Organize answers
-            user_answers = serverinfo["Answers"] # snapshot serverinfo["Answers"] at this point in time
-                                                 # to ignore new answers that are added to it
+            user_answers = channelinfo["Answers"] # snapshot channelinfo["Answers"] at this point in time
+                                                  # to ignore new answers that are added to it
             answerdict = {["a", "b", "c", "d"][num]: answers[num] for num in range(4)}
 
             # Check for AFK
             if len(user_answers) < 2:
                 afk_questions += 1
                 if afk_questions == 3:
-                    await self.bot.send_message(server, "The game has been cancelled due "
+                    await self.bot.send_message(channel, "The game has been cancelled due "
                                                 "to lack of participation.")
-                    self.playing_servers.pop(server.id)
+                    self.playing_channels.pop(channel.id)
                     return
             else:
                 afk_questions = 0
@@ -175,8 +174,8 @@ class Quiz:
                     break
             assert answerdict[correct_letter] == html.unescape(dictionary["correct_answer"])
             message = "Correct answer:```{}. {}```".format(correct_letter.upper(),
-                                                           dictionary["correct_answer"])
-            await self.bot.send_message(server, message)
+                                                           answerdict[correct_letter])
+            await self.bot.send_message(channel, message)
 
             # Assign scores
             for playerid in user_answers:
@@ -184,44 +183,44 @@ class Quiz:
                     time_taken = user_answers[playerid]["Time"] - start_time
                     assert time_taken > 0
                     if time_taken < 1:
-                        serverinfo["Players"][playerid] += 1000
+                        channelinfo["Players"][playerid] += 1000
                     else:
                         # the 20 in the formula below is 2 * 10s (max answer time)
-                        serverinfo["Players"][playerid] += round(1000 * (1 - (time_taken / 20)))
+                        channelinfo["Players"][playerid] += round(1000 * (1 - (time_taken / 20)))
 
             # Display top 5 players and their points
-            message = self.scoreboard(server)
-            await self.bot.send_message(server, "Scoreboard:\n" + message)
+            message = self.scoreboard(channel)
+            await self.bot.send_message(channel, "Scoreboard:\n" + message)
             await asyncio.sleep(4)
 
             if index < 19:
-                await self.bot.send_message(server, "Next question...")
+                await self.bot.send_message(channel, "Next question...")
                 await asyncio.sleep(1)
 
         # Ending and Results
-        await self.end_game(server)
+        await self.end_game(channel)
 
-    async def end_game(self, server):
+    async def end_game(self, channel):
         """Ends a quiz game."""
         # non-linear credit earning .0002x^{2.9} where x is score/100
         # leaderboard with credits earned
-        serverinfo = self.playing_servers[server.id]
-        idlist = sorted(list(serverinfo["Players"]),
-                        key=(lambda idnum: serverinfo["Players"][idnum]),
+        channelinfo = self.playing_channels[channel.id]
+        idlist = sorted(list(channelinfo["Players"]),
+                        key=(lambda idnum: channelinfo["Players"][idnum]),
                         reverse=True)
 
-        winner = server.get_member(idlist[0])
-        await self.bot.send_message(server, "Game over! {} won!".format(winner.mention))
+        winner = channel.server.get_member(idlist[0])
+        await self.bot.send_message(channel, "Game over! {} won!".format(winner.mention))
 
         bank = self.bot.get_cog("Economy").bank
-        leaderboard = "```json\n"
-        max_credits = self.calculate_credits(serverinfo["Players"][idlist[0]])
+        leaderboard = "```py\n"
+        max_credits = self.calculate_credits(channelinfo["Players"][idlist[0]])
         end_len = len(str(max_credits)) + 1 # the 1 is for a space between a max length name and the score
-        rank_len = len(str(len(serverinfo["Players"])))
+        rank_len = len(str(len(channelinfo["Players"])))
         rank = 1
         no_account = False
         for playerid in idlist:
-            player = server.get_member(playerid)
+            player = channel.server.get_member(playerid)
             account_exists = bank.account_exists(player) # how does this know what server it's called in???
 
             if account_exists:
@@ -238,7 +237,7 @@ class Quiz:
             leaderboard += str(rank)
             leaderboard += " " * (1 + rank_len - len(str(rank)))
             leaderboard += name
-            creds = self.calculate_credits(serverinfo["Players"][playerid])
+            creds = self.calculate_credits(channelinfo["Players"][playerid])
             creds_str = str(creds)
             leaderboard += " " * (26 - rank_len - 1 - len(name) - len(creds_str))
             leaderboard += creds_str + "\n"
@@ -256,27 +255,27 @@ class Quiz:
             leaderboard += ("* because you do not have a bank account, "
                             "you did not get to keep the credits you won.```\n")
 
-        await self.bot.send_message(server, "Credits earned:\n" + leaderboard)
-        self.playing_servers.pop(server.id)
+        await self.bot.send_message(channel, "Credits earned:\n" + leaderboard)
+        self.playing_channels.pop(channel.id)
 
-    def scoreboard(self, server):
+    def scoreboard(self, channel):
         """Returns a scoreboard string to be sent to the text channel."""
-        serverinfo = self.playing_servers[server.id]
-        scoreboard = "```json\n"
-        idlist = sorted(list(serverinfo["Players"]),
-                        key=(lambda idnum: serverinfo["Players"][idnum]),
+        channelinfo = self.playing_channels[channel.id]
+        scoreboard = "```py\n"
+        idlist = sorted(list(channelinfo["Players"]),
+                        key=(lambda idnum: channelinfo["Players"][idnum]),
                         reverse=True)
-        max_score = serverinfo["Players"][idlist[0]]
+        max_score = channelinfo["Players"][idlist[0]]
         end_len = len(str(max_score)) + 1
         rank = 1
         for playerid in idlist[:5]:
-            player = server.get_member(playerid)
+            player = channel.server.get_member(playerid)
             if len(player.display_name) > 24 - end_len:
                 name = player.display_name[:21 - end_len] + "..."
             else:
                 name = player.display_name
             scoreboard += str(rank) + " " + name
-            score_str = str(serverinfo["Players"][playerid])
+            score_str = str(channelinfo["Players"][playerid])
             scoreboard += " " * (24 - len(name) - len(score_str))
             scoreboard += score_str + "\n"
             rank += 1
